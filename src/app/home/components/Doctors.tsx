698faae26e956b5da422e3622135d1cd6fa9a375
priya05122000 <npriyanagendran2000@gmail.com>
2026-02-27 13:30:57 +0530

    fixes

--- src/app/home/components/Doctors.tsx
+++ src/app/home/components/Doctors.tsx
@@ -7,102 +7,125 @@ const Doctors = () => {
     return (
         <div className='py-10 sm:py-16'>
             <CenterSection>
-                <div className='grid grid-cols-2'>
-                    <div className='bg-dark/10 p-4'>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe deleniti adipisci similique nobis! Officiis quod quo magni molestias similique pariatur nemo ad iure alias maiores, odio accusantium mollitia tempora. Maiores?
-                    </div>
-                    <div className='bg-primary-light p-4'>
 
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe deleniti adipisci similique nobis! Officiis quod quo magni molestias similique pariatur nemo ad iure alias maiores, odio accusantium mollitia tempora. Maiores?
+                {/* Top Text Grid */}
+                <div className='grid grid-cols-2 h-32 '>
+                    <div className=' p-4'>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        Saepe deleniti adipisci similique nobis! Officiis quod quo
+                        magni molestias similique pariatur nemo ad iure alias maiores.
+                    </div>
+                    <div className='flex'>
+
+                        <div className='bg-primary-light p-4 h-full w-2/3 '>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, non!
+                        </div>
+                        <div className='bg-primary w-1/3 h-full flex  text-center justify-center items-center p-4'>
+                            Lorem ipsum dolor sit
+                        </div>
                     </div>
                 </div>
-                <div className='grid grid-cols-2 relative'>
-                    <div>
-                        <div className='flex'>
-                            <div className='bg-primary/10  h-32 w-40 '>
+
+                {/* Image + Cards Grid */}
+                <div className='grid grid-cols-2 relative '>
+
+                    {/* LEFT SIDE */}
+                    <div className='h-full flex flex-col'>
+
+                        {/* Small top card */}
+                        <div className='flex '>
+                            <div className=' h-32 w-1/3'>
                                 <Image
-                                    src="/services/dental-clinic.webp"
+                                    src="/doctors/doctor-holds-prosthesis.jpg"
                                     width={500}
                                     height={500}
-                                    alt="Picture of the author"
-                                    className='w-full h-full object-cover'
+                                    alt="doctor"
+                                    className='w-full h-full object-cover object-top'
                                 />
                             </div>
-                            <div className='bg-primary/10 p-4 h-32 w-full'>
+                            <div className='bg-dark text-light p-4 h-32 w-[calc(100%-33.33%)]'>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, non!
                             </div>
                         </div>
 
-                        <div className='relative'>
+                        {/* Big middle image (auto stretch) */}
+                        <div className='relative flex-1'>
                             <Image
-                                src="/services/dental-clinic.webp"
+                                src="/doctors/smiling-woman-dentist-chair.jpg"
                                 width={500}
                                 height={500}
-                                alt="Picture of the author"
+                                alt="dentist"
                                 className='w-full h-full object-cover'
                             />
-
                         </div>
+
                     </div>
 
-                    <div className='bg-primary-light '>
+                    {/* RIGHT SIDE BIG IMAGE */}
+                    <div className='h-128 '>
                         <Image
-                            src="/services/dental-clinic.webp"
+                            src="/doctors/close-up-female-1.jpg"
                             width={500}
                             height={500}
-                            alt="Picture of the author"
+                            alt="close up"
                             className='w-full h-full object-cover'
                         />
                     </div>
 
+                    {/* FLOATING CARD 1 */}
                     <div className='absolute top-32 w-1/2 left-1/3'>
                         <div className='flex'>
-                            <div className='bg-primary/10  h-32 w-40 '>
+                            <div className=' h-32 w-1/3'>
                                 <Image
-                                    src="/services/dental-clinic.webp"
+                                    src="/doctors/portrait-man-doctor.jpg"
                                     width={500}
                                     height={500}
-                                    alt="Picture of the author"
-                                    className='w-full h-full object-cover'
+                                    alt="doctor"
+                                    className='w-full h-full object-cover object-top'
                                 />
                             </div>
-                            <div className='bg-primary p-4 h-32 w-full'>
+                            <div className='bg-primary-light p-4 h-32 w-2/3 text-white'>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, non!
                             </div>
                         </div>
                     </div>
+
+                    {/* FLOATING CARD 2 */}
                     <div className='absolute top-64 w-1/2 left-1/3'>
                         <div className='flex'>
-                            <div className='bg-primary/10  h-32 w-40 '>
+                            <div className=' h-32 w-1/3'>
                                 <Image
-                                    src="/services/dental-clinic.webp"
+                                    src="/doctors/female-doctor.jpg"
                                     width={500}
                                     height={500}
-                                    alt="Picture of the author"
-                                    className='w-full h-full object-cover'
+                                    alt="doctor"
+                                    className='w-full h-full object-cover object-top'
                                 />
                             </div>
-                            <div className='bg-primary p-4 h-32 w-full'>
+                            <div className='bg-dark p-4 h-32 w-2/3 text-white'>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, non!
                             </div>
                         </div>
                     </div>
-                    <div className='absolute top-96 w-1/2 left-40'>
+
+                    {/* FLOATING CARD 3 */}
+                    <div className='absolute top-96 w-1/2 right-1/3'>
                         <div className='flex'>
-                            <div className='bg-primary/10  h-32 w-40 '>
+                            <div className='bg-primary/10 h-32 w-1/3'>
                                 <Image
-                                    src="/services/dental-clinic.webp"
+                                    src="/doctors/doctor-with-his-arms.jpg"
                                     width={500}
                                     height={500}
-                                    alt="Picture of the author"
-                                    className='w-full h-full object-cover'
+                                    alt="doctor"
+                                    className='w-full h-full object-cover object-top'
                                 />
                             </div>
-                            <div className='bg-primary p-4 h-32 w-full'>
+                            <div className='bg-primary p-4 h-32 w-2/3 text-white'>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, non!
                             </div>
                         </div>
                     </div>
+
                 </div>
 
             </CenterSection>
