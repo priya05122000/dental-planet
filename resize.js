b1942f104643c8e1f7b19ce26a839a6258420518
priya05122000 <npriyanagendran2000@gmail.com>
2026-03-05 16:50:37 +0530

    fixes

--- resize.js
+++ resize.js
@@ -16,16 +16,16 @@
 const sharp = require("sharp");
 
 sharp("public/design/bannerteeth.png")
-  .resize({
-    width: 400,
-    height: 400,
-    fit: "cover",
-    // position:"top"
-  })
+  // .resize({
+  //   width: 800,
+  //   height: 800,
+  //   fit: "cover",
+  //   position:"top"
+  // })
   .webp({
-    quality: 100,
+    quality: 80,
     effort: 6
   })
-  .toFile("public/casestudy/bannerteeth.webp")
+  .toFile("public/design/bannerteeth.webp")
   .then(() => console.log("✅ Done"))
   .catch(console.error);
